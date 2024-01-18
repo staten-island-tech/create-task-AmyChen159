fa20dc03dde3f314228aad3bf95cf98e91285f74
AmyChen159 <144455940+AmyChen159@users.noreply.github.com>
2024-01-18 09:24:22 -0500

    aDFGHGFSDA

---
+++ vite-project/js/dom.js
@@ -0,0 +1,7 @@
+const DOMselectors = {
+    task: document.getElementById("input"),
+    date: document.getElementById("input2"),
+    submit: document.getElementById("submit"),
+}
+
+export {DOMselectors};
