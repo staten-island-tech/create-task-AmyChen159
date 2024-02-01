0d5c8a167bfb115526bfc624bf110984a7a4daa1
AmyChen159 <144455940+AmyChen159@users.noreply.github.com>
2024-02-01 00:35:39 -0500

    n

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -4,16 +4,20 @@ import { DOMselectors } from './dom';
 const tasks = []
 
 function addTask(){
-    tasks.push(DOMselectors.task.value)
+    tasks.push(DOMselectors.task.value);
+    DOMselectors.task.value = '';
 }
 
 function createCard(){
-
+    tasks.forEach(task => {
+        document.querySelector("#container").insertAdjacentHTML('beforeend',
+            `<div id="gallery">
+                <h2>${task}</h2>
+            </div>` 
+         );
+    });
 }
 
-function clear(){
-    DOMselectors.task.innerHTML = "";
-}
 
 DOMselectors.submit.addEventListener('click', function(){
     addTask()
